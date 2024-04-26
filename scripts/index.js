0055adfe3373e806e166bdbbb20cccc400feb791
Brian Bird <ProfBird@users.noreply.github.com>
2024-04-26 13:52:17 -0700

    Refactored to use arrow functions for event handlers

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 /* I/O code for the Going to Boston dice game.
-   Written by Brian Bird, 4/16/2024, revised 4/22/24 */
+   Written by Brian Bird, 4/16/2024, revised 4/26/24 */
 
 // Global constants
 const NUMBER_OF_DIE = 3;
@@ -7,22 +7,19 @@ const NUMBER_OF_SIDES = 6;   // number of die images--representing the sides of 
 
 let boston = new Game();
 
-// Initialize the page
-
-// disable all buttons except start
+// Initialize the web page
 document.getElementById("endTurn").disabled = true;
 document.getElementById("endRound").disabled = true;
 document.getElementById("roll").disabled = true;
 enableSetAside(false);
 
-// Event handlers for the bottons
-document.getElementById("start").addEventListener("click", start);
-document.getElementById("roll").addEventListener("click", rollDice);
-document.getElementById("endTurn").addEventListener("click", endTurn);
-document.getElementById("endRound").addEventListener("click", endRound);
+/***************************************************/
+/******  Click Event handlers for the buttons ******/
+/***************************************************/
 
-// add players and start the game
-function start() {
+// start button handler
+document.getElementById("start").onclick = () => {
+    // this handler adds players and starts the game
     let player1Name = document.getElementById("player1Name").value;
     let player2Name = document.getElementById("player2Name").value;
     if (player1Name === "" || player2Name === "") {
@@ -39,9 +36,8 @@ function start() {
     }
 }
 
-// event handlers
-
-function rollDice() {
+// roll button handler
+document.getElementById("roll").onclick = () => {
     boston.rollDice();
     // set the images to the dice values
     let values = boston.getDiceValues();
@@ -53,6 +49,43 @@ function rollDice() {
     document.getElementById("roll").disable = true;
 }
 
+// endTurn button handler
+document.getElementById("endTurn").onclick = () => {
+     // enable the End Round button if player 2's turn ended
+     if (boston.getCurrentPlayer().number === 2) {
+        document.getElementById("endRound").disabled = false;
+        document.getElementById("roll").disabled = true;
+    }
+    else {
+        document.getElementById("roll").disabled = false;
+    }
+    boston.endTurn();  // will switch players and reset the dice
+    document.getElementById("points").textContent = "";
+    document.getElementById("player").textContent = boston.getCurrentPlayer().name;
+    // clear the score dice images
+    for (let i = 0; i < NUMBER_OF_DIE; i++) {
+        document.getElementById(`s${i}`).src = "";
+    }
+    displayDice();
+    // disable the end turn button
+    document.getElementById("endTurn").disabled = true;
+
+    displayScores();
+}
+
+// endRound button handler
+document.getElementById("endRound").onclick = () => {
+    document.getElementById("round").textContent = boston.endRound();
+    document.getElementById("roll").disabled = false;
+    document.getElementById("endRound").disabled = true;
+    displayScores();
+}
+
+/*******************************************************/
+/******************  Helper functions  *****************/
+/*******************************************************/
+
+// Move die from the roll die area to the score die area
 function setAside() {
     // remove the die from the dice array and add it to the scoreDice array 
     let value = boston.setDieAside(Number(this.id));
@@ -74,7 +107,7 @@ function setAside() {
 
 }
 
-// helper function to enable or disable the click event on dice images
+// Enable or disable the click event on dice images
 function enableSetAside(canSetAside) {
     // Event handlers for the dice images so they can be clicked and set aside die for scoring
     for (let i = 0; i < NUMBER_OF_DIE; i++) {
@@ -91,39 +124,8 @@ function enableSetAside(canSetAside) {
 }
 
 
-function endTurn() {
-    // enable the End Round button if player 2's turn ended
-    if (boston.getCurrentPlayer().number === 2) {
-        document.getElementById("endRound").disabled = false;
-        document.getElementById("roll").disabled = true;
-    }
-    else {
-        document.getElementById("roll").disabled = false;
-    }
-    boston.endTurn();  // will switch players and reset the dice
-    document.getElementById("points").textContent = "";
-    document.getElementById("player").textContent = boston.getCurrentPlayer().name;
-    // clear the score dice images
-    for (let i = 0; i < NUMBER_OF_DIE; i++) {
-        document.getElementById(`s${i}`).src = "";
-    }
-    displayDice();
-    // disable the end turn button
-    document.getElementById("endTurn").disabled = true;
 
-    displayScores();
-}
-
-// end the current round
-function endRound() {
-    document.getElementById("round").textContent = boston.endRound();
-    document.getElementById("roll").disabled = false;
-    document.getElementById("endRound").disabled = true;
-    displayScores();
-}
-
-
-// helper function to display the dice values
+// Display the dice values
 function displayDice() {
     let values = boston.getDiceValues();
     for (let i = 0; i < NUMBER_OF_DIE; i++) {
@@ -145,6 +147,7 @@ function displayScores() {
         document.getElementById(`player${i + 1}RoundWins`).textContent = boston.players[i].roundWins;
     }
 }
+
 
 /*******************************/
 /******** Just for Testing *****/
